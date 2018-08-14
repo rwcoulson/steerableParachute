8c7f19cdd88d9fafe339811fcb086906078a72f7
Bob Coulson <rcoulson@Roberts-MacBook-Pro.local>
2018-08-14 13:29:20 -0500

    error log test

--- test.py
+++ test.py
@@ -6,13 +6,13 @@ s = state()
 
 for i in range(20):
 	try:
-		t = time()
 		s.measure()
 		s.Amatrix()
 		R = np.eye(6)
 		B = np.zeros([6,1])
 		Y = np.array([[1,1,1,0,0,0]]).T
 		s.kFilter(s.X, s.P, s.A, B, R, Y)
+		s.logError()
 		print(s.X)
 		sleep(0.5)
 	except KeyboardInterrupt:
